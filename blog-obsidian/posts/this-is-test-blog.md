82e85e855603fc36ff28fa389d54ac1e117e5b30
MyStic <khanh.votuan@hcmut.edu.vn>
2025-03-16 21:27:57 +0700

    init

---
+++ blog-obsidian/posts/this-is-test-blog.md
@@ -0,0 +1,10 @@
+---
+title: How I make this blog with obsidian
+description: How i make this blog with obsidian
+tags:
+  - Obsidian
+date: 2025-03-27
+public: "1"
+---
+## Hi how are you today
+Today, I want to introduce this blog, how I make this blog with react and nodejs backend which using obsidian as an editor
---
+++ index.ts
@@ -0,0 +1,58 @@
+import express, { type Express, type Request, type Response } from "express";
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+import fm from "front-matter";
+import cors from "cors";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+const postsDirectory: string = path.join(__dirname, "blog-obsidian/posts");
+
+interface PostFrontMatter {
+  title: string;
+  description: string;
+  tags: string[];
+  date: Date;
+}
+
+app.use(cors());
+
+app.get("/api/posts", (req: Request, res: Response) => {
+  const files = fs.readdirSync(postsDirectory);
+
+  const posts = files
+    .filter((file) => file.endsWith(".md"))
+    .map((file) => {
+      const filePath = path.join(postsDirectory, file);
+      const content = fs.readFileSync(filePath, "utf-8");
+      const { attributes } = fm<PostFrontMatter>(content);
+
+      return {
+        title: attributes.title,
+        description: attributes.description,
+        tags: attributes.tags,
+        date: attributes.date,
+        slug: file.replace(".md", ""),
+      };
+    });
+  res.send({ posts });
+});
+
+app.get("/api/posts/:slug", (req: Request, res: Response) => {
+  const { slug } = req.params;
+  const filePath = path.join(postsDirectory, `${slug}.md`);
+  const content = fs.readFileSync(filePath, "utf-8");
+  const { body } = fm(content);
+
+  res.send({
+    content: body,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
