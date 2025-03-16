139b68bb4c238866846db6a5c7aaed203a91213d
MyStic <khanh.votuan@hcmut.edu.vn>
2025-03-16 21:39:29 +0700

    chore(deploy): config

--- api/index.ts
+++ api/index.ts
@@ -56,3 +56,5 @@ app.get("/api/posts/:slug", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+module.exports = app;
